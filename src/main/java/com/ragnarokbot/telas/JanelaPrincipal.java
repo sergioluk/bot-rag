3cc6c9b492eae2cb9bd7aafadfab41c33275e4a0
Sergio Rocha <sergiorsj1@hotmail.com>
2025-03-22 23:45:04 -0300

    possivel comecar o farme de qualquer lugar e adicionar notebook do luk

--- src/main/java/com/ragnarokbot/telas/JanelaPrincipal.java
+++ src/main/java/com/ragnarokbot/telas/JanelaPrincipal.java
@@ -197,7 +197,8 @@ public class JanelaPrincipal extends JFrame  implements NativeKeyListener {
         		"FEABFE60-ADC3-79BD-36A2-107C61A5EB2D", //Pc meu
         		"03000200-0400-0500-0006-000700080009", //Xeon do lucas
         		"EC7BC152-5DAC-11EB-1DB2-706979ABEE0D", //Notebook do Luk
-        		"03560274-043C-05BA-4306-910700080009" //Pczao do Luk
+        		"03560274-043C-05BA-4306-910700080009", //Pczao do Luk
+        		"D31B5000-77D6-11E9-B132-AE02394E1000" //Outro notebook escravo do Luk
         		).collect(Collectors.toList());
         for (String id : uuidPermitidos) {
         	if (id.equals(UUID)) {
